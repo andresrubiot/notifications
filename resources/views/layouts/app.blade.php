f8849f821204a86949258f84e0a7f3b8bdb191f3
Andres Rubio <andresrubiot5@gmail.com>
2021-06-13 15:11:10 -0500

    Layout template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,17 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-iten">
+                                <a href="#" class="nav-link">Send message</a>
+                            </li>
+
+                            <li class="nav-iten">
+                                <a href="#" class="nav-link">
+                                    Notifications
+                                    <span class="badge badge-secondary">1</span>
+                                </a>
+                            </li>
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
